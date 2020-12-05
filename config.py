74e7e5ec3dc8c5e11f3d1ba8c9fd026302af8489
mindwanders1993 <biswajit.brahmma@gmail.com>
2020-12-05 09:31:05 +0530

    migration and sqlite3 deleted

--- config.py
+++ config.py
@@ -7,8 +7,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = 'smtp.mailtrap.io'
